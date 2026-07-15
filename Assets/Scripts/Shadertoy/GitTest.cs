f323e39d57c25b7a983ad332e0b60c262b97840f
Heart1901 <mawei_0502@163.com>
2026-07-15 10:32:00 +0800

    新增测试脚本
    新增测试脚本GitTest

---
+++ Assets/Scripts/Shadertoy/GitTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GitTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("测试脚本");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
